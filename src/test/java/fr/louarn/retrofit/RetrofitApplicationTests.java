fa7aeb5138730cbbc7c72dd7c9e4cb72fc3b8e1f
Robin LOUARN <robin.louarn@icloud.com>
2019-06-10 00:23:29 +0200

    utilisation du générateur de service dans les tests

--- src/test/java/fr/louarn/retrofit/RetrofitApplicationTests.java
+++ src/test/java/fr/louarn/retrofit/RetrofitApplicationTests.java
@@ -1,8 +1,8 @@
 package fr.louarn.retrofit;
 
+import fr.louarn.retrofit.config.retrofit.GitHubServiceGenerator;
 import fr.louarn.retrofit.modele.User;
 import fr.louarn.retrofit.service.IUserService;
-import okhttp3.OkHttpClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,16 +17,10 @@ public class RetrofitApplicationTests {
 
     @Test
     public void testCallSync() {
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(httpClient.build())
-                .build();
 
-
-        IUserService service = retrofit.create(IUserService.class);
+        IUserService service = GitHubServiceGenerator.createService(IUserService.class);
         Call<User> callSync = service.getUser("eugenp");
+
 
         try {
             Response<User> response = callSync.execute();
@@ -40,15 +32,8 @@ public class RetrofitApplicationTests {
 
     @Test
     public void testCallAsync() {
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(httpClient.build())
-                .build();
 
-
-        IUserService service = retrofit.create(IUserService.class);
+        IUserService service = GitHubServiceGenerator.createService(IUserService.class);
         Call<User> callAsync = service.getUser("eugenp");
 
         callAsync.enqueue(new Callback<User>() {
